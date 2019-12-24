00553ad4423f707528ed566e5f53fb9120971da3
ANANYA SONKAR <55709811+find-your-fire@users.noreply.github.com>
2019-12-24 20:36:28 +0530

    Update sensor.js

--- sensor.js
+++ sensor.js
@@ -2,7 +2,6 @@ setChartLibrary('google-chart');
 setChartTitle("Sensors:DATA");
 var Graph1=new boltGraph();
 Graph1.setChartType('tableGraph');
-mul(0.0977);
 Graph1.plotChart('time_stamp','detect'); 
 var Graph2=new boltGraph();
 Graph2.setChartType('lineGraph');
